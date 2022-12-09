fe405b667bd1ee82ce479b8bd8af2433107d183e
Donguk <95donguk.dev@gmail.com>
2022-12-09 21:25:20 +0900

    refactor(BridgeMaker): 다리 만들기를 스트림으로 코드 리팩토링

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,10 +1,9 @@
 package bridge;
 
-import static bridge.utils.command.MoveCommand.DOWN;
-import static bridge.utils.command.MoveCommand.UP;
-
-import java.util.ArrayList;
+import bridge.utils.command.MoveCommand;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -22,15 +21,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        List<String> bridge = new ArrayList<>();
-
-        for (int bridgeIndex = 0; bridgeIndex < size; bridgeIndex++) {
-            if (DOWN.equalNumber(bridgeNumberGenerator.generate())) {
-                bridge.add(DOWN.getCommand());
-                continue;
-            }
-            bridge.add(UP.getCommand());
-        }
-        return bridge;
+        return Stream.generate(bridgeNumberGenerator::generate)
+                .limit(size)
+                .map(MoveCommand::convert)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
--- src/main/java/bridge/utils/command/MoveCommand.java
+++ src/main/java/bridge/utils/command/MoveCommand.java
@@ -1,10 +1,13 @@
 package bridge.utils.command;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public enum MoveCommand {
     UP(1, "U"),
     DOWN(0, "D");
+
+    private static final String INVALID_NUMBER_MESSAGE = "유효하지 않은 숫자 값 입니다.";
 
     private final int number;
     private final String command;
@@ -12,6 +15,14 @@ public enum MoveCommand {
     MoveCommand(int number, String command) {
         this.number = number;
         this.command = command;
+    }
+
+    public static String convert(int number) {
+        return Arrays.stream(values())
+                .filter(moveCommand -> Objects.equals(moveCommand.number, number))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_NUMBER_MESSAGE))
+                .command;
     }
 
     public String getCommand() {
